53540f5147bfa54214e10c0fb8e7e47eedb3af81
gloria larbi <Miss-Gloria>
2025-02-25 19:07:07 +0000

    more updates

--- js/app.js
+++ js/app.js
@@ -14,3 +14,23 @@
   window.onload = function () {
     typeWriter();
   };
+  document.addEventListener("DOMContentLoaded", () => {
+    taos.init();
+});
+
+// const carousel = document.getElementById("carousel");
+// const images = carousel.children;
+// const totalSlides = images.length;
+// let index = 0;
+
+// function autoSlide() {
+//   index = (index + 1) % totalSlides;
+//   updateCarousel();
+// }
+
+// function updateCarousel() {
+//   carousel.style.transform = `translateX(-${index * 100}%)`;
+// }
+
+// // Auto-slide every 3 seconds
+// setInterval(autoSlide, 2000);
