f9be1945c4a5898cffb251a9f09f8582d8f0381b
gloria larbi <Miss-Gloria>
2025-02-26 19:01:53 +0000

    updated code

--- js/app.js
+++ js/app.js
@@ -34,3 +34,17 @@
 
 // // Auto-slide every 3 seconds
 // setInterval(autoSlide, 2000);
+  document.querySelectorAll(".faq-btn").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const answer = btn.nextElementSibling;
+      const icon = btn.querySelector(".faq-icon");
+
+      if (answer.classList.contains("hidden")) {
+        answer.classList.remove("hidden");
+        icon.innerHTML = "▼"; // Change to down arrow
+      } else {
+        answer.classList.add("hidden");
+        icon.innerHTML = "➤"; // Change back to right arrow
+      }
+    });
+  });
